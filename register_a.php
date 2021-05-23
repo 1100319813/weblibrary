68e2e6f63427fb0f919575fd4f822ed2dba8df87
1100319813 <44244357+1100319813@users.noreply.github.com>
2021-05-23 05:29:38 -0400

    Update register_a.php

--- register_a.php
+++ register_a.php
@@ -1,10 +1,19 @@
+
 <?php
-extract($_POST);
-include("database.php");
-$sql=mysqli_query($conn,"SELECT * FROM users where email='$email'");
-$query="INSERT INTO register(user_id, name, grade, email, phone, password, admin ) VALUES ('$id', '$first_name', '$grade', '$email', '$phone', 'md5($pass)', '$admin')";
-$sql=mysqli_query($conn,$query)or die("Could Not Perform the Query");
-header ("Location: login.php?status=success");
 
+if ($_POST["password"] == $_POST["cpass"]) {
 
+include 'database.php';
+$sql = "INSERT INTO users (user_id, name, grade, email, phone, password, admin) VALUES ('".$_POST["user_id"]."','".$_POST["name"]."','".$_POST["grade"]."','".$_POST["email"]."','".$_POST["phone"]."','".$_POST["password"]."','0')"; 
+
+$result=mysqli_query($conn,$sql)or die("Could Not Perform the Query");
+
+header("Location: login.php"); 
+
+mysqli_close($conn); //Make sure to close out the database connection
+}
+else {
+	echo "password don't match";
+}
 ?>
+
